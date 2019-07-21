f82b62abe881f84756107f04e1bbc3dbaf0d1d27
wangyi <1121995621@qq.com>
2019-07-21 17:11:36 +0800

    update 7-21

--- app/src/main/java/com/wang/faceidtest2/MainActivity.java
+++ app/src/main/java/com/wang/faceidtest2/MainActivity.java
@@ -2,19 +2,30 @@ package com.wang.faceidtest2;
 
 import android.Manifest;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.ZoomControls;
 
 import com.baidu.location.BDAbstractLocationListener;
 import com.baidu.location.BDLocation;
@@ -41,39 +52,60 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import top.zibin.luban.Luban;
+import top.zibin.luban.OnCompressListener;
+import top.zibin.luban.OnRenameListener;
 
 public class MainActivity extends AppCompatActivity {
     private static final int TAKE_PHOTO = 1;
     private static final int LOGIN_SUCCESS = 2;
     private static final int LOGIN_ERROR = 3;
     private static final String TAG = "MainActivity";
-    private TextView locationMsg;
     private Uri imageUri;
-    private static final String ServerIP = "http://10.0.2.2:8888";
+    private static final String ServerAddr = "https://172.16.183.181:8888";
     private ProgressDialog mProgressDialog;
     private String imagePath_take;
     public LocationClient mLocationClient;
     private MapView mMapView;
     private BaiduMap baiduMap;
+    private FloatingActionButton mFloatingActionButton;
     private boolean isFirstLocate = true;//用来显示是否是第一次定位
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);//全屏显示（隐藏状态栏）*/
-        mProgressDialog = new ProgressDialog(MainActivity.this);
-        mProgressDialog.setMessage("正在识别，请稍后...");
-        mProgressDialog.setCancelable(false);
-        //Button button = findViewById(R.id.id_login);
-        //locationMsg = findViewById(R.id.location);
+        SDKInitializer.initialize(getApplicationContext());//这句话需要加在setContentView之前
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar =findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);//禁止手动滑动弹出
+        //mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);//打开手动滑动弹出
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
 
+
+        mProgressDialog = new ProgressDialog(MainActivity.this);
+        mProgressDialog.setCancelable(false);
+        mProgressDialog.setMessage("正在上传图片，请稍后...");
+        mFloatingActionButton = findViewById(R.id.login);
 
         mLocationClient = new LocationClient(getApplicationContext());
         mLocationClient.registerLocationListener(new MyLocationListener());
-        SDKInitializer.initialize(getApplicationContext());
-        setContentView(R.layout.activity_main);
+
         mMapView= findViewById(R.id.bdmapView);
+        //隐藏baidu的logo
+        View child = mMapView.getChildAt(1);
+        if (child != null && (child instanceof ImageView || child instanceof ZoomControls)){
+            child.setVisibility(View.INVISIBLE);
+        }
+        mMapView.showZoomControls(false);//放大缩小控件
+        mMapView.showScaleControl(true);//比例尺控件
+
         baiduMap = mMapView.getMap();
         baiduMap.setMyLocationEnabled(true);
 
@@ -95,9 +127,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        /*button.setOnClickListener(new View.OnClickListener() {
+        mFloatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //Toast.makeText(getApplicationContext(),"点击!" ,Toast.LENGTH_SHORT).show();
                 String imagename="face.jpg";
                 File outputImage=new File(getExternalCacheDir(),imagename);//创建File对象，用于存储拍照后的照片
                 imagePath_take=getExternalCacheDir()+"/"+imagename;
@@ -117,14 +150,11 @@ public class MainActivity extends AppCompatActivity {
                 //启动相机程序
                 Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri );
-                intent.putExtra("camerasensortype", 2); // 调用前置摄像头
-                intent.putExtra("autofocus", true); // 自动对焦
-                intent.putExtra("fullScreen", false); // 全屏
-                intent.putExtra("showActionIcons", false);
                 startActivityForResult(intent,TAKE_PHOTO);
             }
-        });*/
+        });
     }
+
 
     private void requestLocation(){
         LocationClientOption option = new LocationClientOption();
@@ -135,7 +165,27 @@ public class MainActivity extends AppCompatActivity {
         mLocationClient.start();//打开地图定位图层
     }
 
-    private void navigageTo(BDLocation location){
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar,menu );
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.refresh:
+                isFirstLocate = true;
+                mLocationClient.start();//重新定位
+                break;
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+        }
+        return true;
+    }
+
+    private void navigateTo(BDLocation location){
         if(location!=null){
             if (isFirstLocate){
                 LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude() );
@@ -160,6 +210,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
 
     @Override
     protected void onDestroy() {
@@ -207,14 +258,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         switch (requestCode){
-            case TAKE_PHOTO:
+            case TAKE_PHOTO://拍照
                 if (resultCode == RESULT_OK){
                     try{
                         mProgressDialog.show();
-                        /*Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
-                        picture.setImageBitmap(bitmap);*/
                         File file = new File(imagePath_take);
-                        uploadImage(MainActivity.this,ServerIP,file);
+                        resize(file);//压缩图片
+                        uploadImage(ServerAddr,file);
                     }catch (Exception e){
                         e.printStackTrace();
                     }
@@ -224,7 +274,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void uploadImage(final Context context, String url, File file) {
+    private void askForResult(){
+                try{
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder()
+                            .url(ServerAddr)
+                            .build();
+                    Response response = client.newCall(request).execute();
+                    String responseData = response.body().string();
+                    Toast.makeText(getApplicationContext(),"识别结果是"+responseData ,Toast.LENGTH_SHORT ).show();
+                    mProgressDialog.dismiss();
+                }catch (Exception e){
+                    e.printStackTrace();
+                    Log.i(TAG,e.getMessage());
+                    Toast.makeText(getApplicationContext(), "发生错误!"+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    mProgressDialog.dismiss();
+                }
+
+    }
+    private void uploadImage(String url, File file) {
         try {
             OkHttpClient mOkHttpClient = new OkHttpClient();
             MultipartBody.Builder builder = new MultipartBody.Builder()
@@ -240,11 +308,12 @@ public class MainActivity extends AppCompatActivity {
             call.enqueue(new Callback() {
                 @Override
                 public void onFailure(Call call, IOException e) {
-                    Log.i(TAG, e.getMessage());
+                    Log.i(TAG, "上传失败:"+e.getMessage());
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            Toast.makeText(context, "上传失败！", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getApplicationContext(), "上传失败,请检查网络连接", Toast.LENGTH_SHORT).show();
+                            mProgressDialog.dismiss();
                         }
                     });
                 }
@@ -255,35 +324,31 @@ public class MainActivity extends AppCompatActivity {
                    runOnUiThread(new Runnable() {
                        @Override
                        public void run() {
-                           Toast.makeText(context, "上传成功", Toast.LENGTH_SHORT).show();
+                           Toast.makeText(getApplicationContext(), "上传成功", Toast.LENGTH_SHORT).show();
+                           mProgressDialog.dismiss();
+                           mProgressDialog.setMessage("正在识别，请稍后...");
+                           mProgressDialog.show();
+                           askForResult();
                        }
                    });
 
                 }
             });
-            //Toast.makeText(context, "上传成功", Toast.LENGTH_SHORT).show();
         } catch (Exception e) {
             e.printStackTrace();
             Log.i(TAG,e.getMessage());
-            Toast.makeText(context, "上传失败!"+e.getMessage(), Toast.LENGTH_SHORT).show();
-        }finally {
+            Toast.makeText(getApplicationContext(), "发生错误!"+e.getMessage(), Toast.LENGTH_SHORT).show();
             mProgressDialog.dismiss();
         }
     }
-
-
-
-
-public class MyLocationListener extends BDAbstractLocationListener {
-
+    public class MyLocationListener extends BDAbstractLocationListener {
     @Override
     public void onReceiveLocation(BDLocation bdLocation) {
-        StringBuilder stringBuilder = new StringBuilder();
+
         if (bdLocation.getLocType()==BDLocation.TypeGpsLocation||bdLocation.getLocType()==BDLocation.TypeNetWorkLocation){
-            navigageTo(bdLocation);
+            navigateTo(bdLocation);
 
         }else {
-            //locationMsg.setText("发生未知错误，定位失败！");
             if (!isFirstLocate){
                 Toast.makeText(getApplicationContext(), "发生未知错误，定位失败！", Toast.LENGTH_SHORT).show();
                 Log.i(TAG,"发生未知错误，定位失败！" );
@@ -291,5 +356,37 @@ public class MyLocationListener extends BDAbstractLocationListener {
         }
     }
 }
+    /**
+     * 压缩图片
+     *
+     */
+    public void resize(final File src) {
+        Luban.with(getApplicationContext()).load(src).ignoreBy(1000).setTargetDir(src.getParent()).setRenameListener(new OnRenameListener() {
+            @Override
+            public String rename(String filePath) {
+                return "face_re.jpg";
+            }
+        }).setCompressListener(new OnCompressListener() {
+            @Override
+            public void onStart() {
+                Log.i(TAG,"开始压缩" );
+                Log.i(TAG,"图片位置："+src.getPath());
+            }
+
+            @Override
+            public void onSuccess(File file) {
+                Log.i(TAG,"压缩成功" );
+                uploadImage(ServerAddr,file);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                File src = new File(imagePath_take);
+                Log.i(TAG,"压缩失败"+e.getMessage());
+                Toast.makeText(getApplicationContext(),"压缩失败，正在上传原图，请耐心等待···" ,Toast.LENGTH_SHORT ).show();
+                uploadImage(ServerAddr,src);
+            }
+        }).launch();
+    }
 
 }
